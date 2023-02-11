578fbe6b142a782215ec1b80c28943c42ab76754
Jones <jmmarao@gmail.com>
2023-02-11 00:41:47 -0300

    Add employee service functions

--- src/main/java/com/jmmarao/employeemanager/service/EmployeeService.java
+++ src/main/java/com/jmmarao/employeemanager/service/EmployeeService.java
@@ -2,9 +2,11 @@ package com.jmmarao.employeemanager.service;
 
 import com.jmmarao.employeemanager.model.Employee;
 import com.jmmarao.employeemanager.repository.EmployeeRepository;
+import com.jmmarao.employeemanager.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +21,21 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + "was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
     }
 }
