11d00aded09f46bc1217d15fac3c0b23a17debc6
Jones <jmmarao@gmail.com>
2023-02-11 00:32:45 -0300

    Add employee service

---
+++ src/main/java/com/jmmarao/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.jmmarao.employeemanager.repository;
+
+import com.jmmarao.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/com/jmmarao/employeemanager/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.jmmarao.employeemanager.service;
+
+import com.jmmarao.employeemanager.model.Employee;
+import com.jmmarao.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+}
